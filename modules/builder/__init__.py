a1a587edd49568dfe6af7653dc19de36e37eeb5f
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-04-03 01:09:08 +0100

    corrected price in jamla file generation (x100) to store a pennies

--- modules/builder/__init__.py
+++ modules/builder/__init__.py
@@ -42,7 +42,9 @@ def save_items():
         item['title'] = getItem(form.title.data, index)
         item['sku'] = getItem(form.title.data, index)
         item['sell_price'] = getItem(form.sell_price.data, index) or 0
+        item['sell_price'] = item['sell_price'] * 100  
         item['monthly_price'] = getItem(form.monthly_price.data, index) or 0
+        item['monthly_price'] = item['monthly_price'] * 100 
         item['subscription'] = getItem(form.subscription.data, index)
         item['instant_payment'] = getItem(form.instant_payment.data, index)
         item['selling_points'] = getItem(form.selling_points.data, index)
