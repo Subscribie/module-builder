b02b249b373365c3940008d97bff8c365343162f
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-04-14 14:48:16 +0100

    added DEPLOYER_SETTINGS loadfromenvvar , parameterized many of the paths to help local development (e.g. APACHE_CONF_FILE env var) added .env.example and .gitkeep for sites folder

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os, shutil, subprocess
+import os, errno, shutil, subprocess
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import git
 
 app = Flask(__name__)
+app.config.from_envvar('DEPLOYER_SETTINGS')
 
 @app.route('/deploy', methods=['GET', 'POST'])
 def deploy():
@@ -16,29 +17,38 @@ def deploy():
         return 'No selected file'
        
     if file:
+        # Store submitted jamla file in its site folder
         filename = secure_filename(file.filename)
-        file.save(os.path.join('/home/subwebbuild/www/sites/', filename))
+        filename = filename.split('.')[0]
+        webaddress = filename.lower() + '.subscriby.shop'
 
         # Create directory for site
-        filename = filename.split('.')[0]
-	webaddress = filename.lower() + '.subscriby.shop'
-        dstDir = '/home/subwebbuild/www/sites/' + webaddress
-        os.mkdir(dstDir)
+        try:
+            dstDir = app.config['SITES_DIRECTORY'] + webaddress + '/'
+            os.mkdir(dstDir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 	# Clone hedgehog repo
-        git.Git(dstDir).clone("git@gitlab.com:karmacrew/hedgehog.git")
-	# Generate .env file
-        shutil.copy2(dstDir + '/hedgehog/shortly/.env.example', dstDir + '/hedgehog/shortly/.env')
+        try:
+            git.Git(dstDir).clone("git@gitlab.com:karmacrew/hedgehog.git")
+        except:
+            pass
+        file.save(os.path.join(dstDir, filename + '.yaml'))
+        # Generate .env file
+        shutil.copy2(dstDir + 'hedgehog/shortly/.env.example', dstDir + 'hedgehog/shortly/.env')
         # Copy Jamla file into repo
-        shutil.move('/home/subwebbuild/' + filename + '.yaml', dstDir + '/jamla.yaml')
+        shutil.move(dstDir + filename + '.yaml', dstDir + 'jamla.yaml')
+        import pdb;pdb.set_trace()
         # Set JAMLA path
-        jamlaPath = dstDir + '/jamla.yaml'
-	fp = open(dstDir + "/hedgehog/shortly/.env", "a+")
+        jamlaPath = dstDir + 'jamla.yaml'
+        fp = open(dstDir + "hedgehog/shortly/.env", "a+")
         fp.write('JAMLA_PATH="' + jamlaPath + '"' + "\n")
         fp.close()
 
         # Append new site to apache config
         vhost = "Use VHost " + webaddress + " subwebbuild /home/subwebbuild/www/sites/" + webaddress
-	fp = open("/home/subwebbuild/www/sites/mass-virtual-hosting-jamla.conf", "a+")
+	fp = open(app.config['APACHE_CONF_FILE'], "a+")
         fp.write(vhost + "\n")
         fp.close()
         # Reload apache with new vhost
