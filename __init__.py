d4cb9180bac17162f02c21bdb4f279d62c29338b
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-17 23:34:43 +0000

    replace deprecated file method with open

--- __init__.py
+++ __init__.py
@@ -117,7 +117,7 @@ def save_items():
 
     subdomain = create_subdomain_string(draftJamla)
     session['site-url'] = 'https://' + subdomain.lower() + '.subscriby.shop'
-    stream = file(subdomain + '.yaml', 'w')
+    stream = open(subdomain + '.yaml', 'w')
     # Save to yml
     yaml.safe_dump(draftJamla, stream,default_flow_style=False)
     # Generate site
